e5673012cfaf3406f7f8df6e6b22d83be87a4212
ibershtein <IgorBershtein@gmail.com>
2016-04-26 12:28:27 +0300

    adding NN example

--- features/step_definitions/sanity2_feature.rb
+++ features/step_definitions/sanity2_feature.rb
@@ -2510,7 +2510,7 @@ Given /^Code Tested2$/  do
   require 'ruby-fann'
   train = RubyFann::TrainData.new(:inputs=>[[0.3, 0.4, 0.5], [0.1, 0.2, 0.3]], :desired_outputs=>[[0.7], [0.8]])
   fann = RubyFann::Standard.new(:num_inputs=>3, :hidden_neurons=>[2, 8, 4, 3, 4], :num_outputs=>1)
-  fann.train_on_data(train, 1000, 10, 0.1) # 1000 max_epochs, 10 errors between reports and 0.1 desired MSE (mean-squared-error)
+  fann.train_on_data(train, 1000, 10, 0.002) # 1000 max_epochs, 10 errors between reports and 0.1 desired MSE (mean-squared-error)
   outputs = fann.run([0.3, 0.2, 0.4])
 
   #Save training data to file and use it later (continued from above)
@@ -2518,12 +2518,42 @@ Given /^Code Tested2$/  do
   train = RubyFann::TrainData.new(:filename=>'verify.train')
   # Train again with 10000 max_epochs, 20 errors between reports and 0.01 desired MSE (mean-squared-error)
   # This will take longer:
-  fann.train_on_data(train, 10000, 20, 0.01)
+  fann.train_on_data(train, 10000, 20, 0.002)
 
 
   #Save trained network to file and use it later (continued from above)
   fann.save('foo.net')
   saved_nn = RubyFann::Standard.new(:filename=>"foo.net")
   saved_nn.run([0.3, 0.2, 0.4])
+
+=begin
+Max epochs     1000. Desired error: 0.0020000001.
+Epochs            1. Current error: 0.0733333007. Bit fail 0.
+Epochs           10. Current error: 0.0026936561. Bit fail 0.
+Epochs           20. Current error: 0.0025214723. Bit fail 0.
+Epochs           30. Current error: 0.0025000230. Bit fail 0.
+Epochs           40. Current error: 0.0024879309. Bit fail 0.
+Epochs           50. Current error: 0.0024654362. Bit fail 0.
+Epochs           59. Current error: 0.0019618918. Bit fail 0.
+Max epochs    10000. Desired error: 0.0020000001.
+Epochs            1. Current error: 0.0017666821. Bit fail 0.
+
+
+  0m0.043s
+  Max epochs     1000. Desired error: 0.1000000015.
+  Epochs            1. Current error: 0.0549894162. Bit fail 0.
+  Max epochs    10000. Desired error: 0.0099999998.
+  Epochs            1. Current error: 0.0213634185. Bit fail 0.
+  Epochs            2. Current error: 0.0025007059. Bit fail 0.
+
+
+Max epochs     1000. Desired error: 0.1000000015.
+Epochs            1. Current error: 0.0515302420. Bit fail 0.
+Max epochs    10000. Desired error: 0.0099999998.
+Epochs            1. Current error: 0.0213228129. Bit fail 0.
+Epochs            2. Current error: 0.0028754869. Bit fail 0.
+
+
+=end
 
 end
