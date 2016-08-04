162ceecaa554d2d96d7f30bb082efaeae6ce5ae2
Igor <promo888@gmail.com>
2016-08-04 19:32:02 +0300

    MultiplyPos on SL changed - to StopLoss - to do SL trading not for LOCKER strat

--- features/step_definitions/debug.rb
+++ features/step_definitions/debug.rb
@@ -665,8 +665,7 @@ def adjustMartinMaEntry(startBarIndex,endBarIndex=nil,random=true,ma_period=50,o
         exit_price=$portfolio['last_long_price'].to_f*(1-$stop_loss_percent)
         #break
         if !(exit_price<bar[LOW_FIELD].to_f )
-          $lot_multiplier+=1
-          createTrade(bar[DATE_FIELD],SELL_FIELD,exit_price,$lot_multiplier,'stoploss SL',bar[HIGH_FIELD].to_f,bar[LOW_FIELD].to_f,nil) #if($portfolio['short_size']>0)
+           createTrade(bar[DATE_FIELD],SELL_FIELD,exit_price,$lot_multiplier,'stoploss SL',bar[HIGH_FIELD].to_f,bar[LOW_FIELD].to_f,nil) #if($portfolio['short_size']>0)
         end
 
       #end
@@ -735,8 +734,7 @@ def adjustMartinMaEntry(startBarIndex,endBarIndex=nil,random=true,ma_period=50,o
           exit_price=$portfolio['last_short_price'].to_f*(1+$stop_loss_percent)
           #break
           if !(exit_price>bar[HIGH_FIELD].to_f) #TODO test slippage & max drawdowns by trades stats NinMax% between Open&Close
-            $lot_multiplier+=1
-            createTrade(bar[DATE_FIELD],BUY_FIELD,exit_price,$lot_multiplier,'stoploss',bar[HIGH_FIELD].to_f,bar[LOW_FIELD].to_f,nil) #if($portfolio['short_size']>0)
+             createTrade(bar[DATE_FIELD],BUY_FIELD,exit_price,$lot_multiplier,'stoploss',bar[HIGH_FIELD].to_f,bar[LOW_FIELD].to_f,nil) #if($portfolio['short_size']>0)
           end
         end
       #end
