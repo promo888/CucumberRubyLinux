7995d13c653bfa6d07514894a8945596cd60b342
Igor <promo888@gmail.com>
2016-08-04 19:45:53 +0300

    MultiplyPos on SL changed - to StopLoss - to do SL trading not for LOCKER strat

--- features/step_definitions/debug.rb
+++ features/step_definitions/debug.rb
@@ -440,7 +440,7 @@ def startMaTrade(bar,bar_index,bars_array,random_order_type=false,ma_field=CLOSE
 end
 
 
-
+#TODO to handle Open and Intrady gaps with multiplier,otherwise SL or Open's gap KILLs when MaxPos
 #TODO Recalc on RealPortfolio amounts unstead of TP percent+EntryExit levels->exact points from Entry
 def createTrade(barDateTime,orderType,price,qty,positionStatus,barHigh,barLow,indicatorsValues)
 
@@ -665,7 +665,7 @@ def adjustMartinMaEntry(startBarIndex,endBarIndex=nil,random=true,ma_period=50,o
         exit_price=$portfolio['last_long_price'].to_f*(1-$stop_loss_percent)
         #break
         if !(exit_price<bar[LOW_FIELD].to_f )
-           createTrade(bar[DATE_FIELD],SELL_FIELD,exit_price,$lot_multiplier,'stoploss SL',bar[HIGH_FIELD].to_f,bar[LOW_FIELD].to_f,nil) #if($portfolio['short_size']>0)
+           createTrade(bar[DATE_FIELD],SELL_FIELD,exit_price,$portfolio['long_size'].to_,'stoploss SL',bar[HIGH_FIELD].to_f,bar[LOW_FIELD].to_f,nil) #if($portfolio['short_size']>0)
         end
 
       #end
@@ -734,7 +734,7 @@ def adjustMartinMaEntry(startBarIndex,endBarIndex=nil,random=true,ma_period=50,o
           exit_price=$portfolio['last_short_price'].to_f*(1+$stop_loss_percent)
           #break
           if !(exit_price>bar[HIGH_FIELD].to_f) #TODO test slippage & max drawdowns by trades stats NinMax% between Open&Close
-             createTrade(bar[DATE_FIELD],BUY_FIELD,exit_price,$lot_multiplier,'stoploss',bar[HIGH_FIELD].to_f,bar[LOW_FIELD].to_f,nil) #if($portfolio['short_size']>0)
+             createTrade(bar[DATE_FIELD],BUY_FIELD,exit_price,$portfolio['short_size'].to_,'stoploss',bar[HIGH_FIELD].to_f,bar[LOW_FIELD].to_f,nil) #if($portfolio['short_size']>0)
           end
         end
       #end
